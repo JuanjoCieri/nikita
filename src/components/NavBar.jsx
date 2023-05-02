42d207d87b7b0012b479a2566ce014a764bcebfe
JuanjoCieri <juin@hotmail.com.ar>
2023-05-01 22:30:32 -0300

    add flowbite

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Dropdown, Avatar, Navbar } from "flowbite-react";
+import pantuflamain from "../images/nikitapijamamain3.png";
 
 export default function NavBar() {
   const [color, setColor] = useState(false);
@@ -14,7 +16,7 @@ export default function NavBar() {
   window.addEventListener("scroll", changeColor);
 
   return (
-    <nav
+    <Navbar
       className={
         color === false
           ? "h-16 flex justify-around items-center w-full fixed top-0 max-w-screen-2xl"
@@ -62,6 +64,30 @@ export default function NavBar() {
       </div>
       <div className="w-1/4 flex justify-around items-center"></div>
       <div className="w-1/4 flex justify-around items-center">
+        <Dropdown
+          arrowIcon={false}
+          inline={true}
+          label={
+            <Avatar
+              alt="User settings"
+              img={pantuflamain}
+              rounded={true}
+              className="w-4"
+            />
+          }
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">asd</span>
+            <span className="block truncate text-sm font-medium">asd</span>
+          </Dropdown.Header>
+          <Dropdown.Item>Perfil de Usuario</Dropdown.Item>
+
+          <Dropdown.Item>Notificaciones</Dropdown.Item>
+          <Dropdown.Item>Mis donaciones</Dropdown.Item>
+
+          <Dropdown.Divider />
+          <Dropdown.Item>Cerrar Sesión</Dropdown.Item>
+        </Dropdown>
         <div>
           <button
             type="button"
@@ -69,24 +95,6 @@ export default function NavBar() {
             id="options-menu"
           >
             <svg
-              width="30"
-              fill="currentColor"
-              height="30"
-              class="text-gray-800"
-              viewBox="0 0 1792 1792"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M1523 1339q-22-155-87.5-257.5t-184.5-118.5q-67 74-159.5 115.5t-195.5 41.5-195.5-41.5-159.5-115.5q-119 16-184.5 118.5t-87.5 257.5q106 150 271 237.5t356 87.5 356-87.5 271-237.5zm-243-699q0-159-112.5-271.5t-271.5-112.5-271.5 112.5-112.5 271.5 112.5 271.5 271.5 112.5 271.5-112.5 112.5-271.5zm512 256q0 182-71 347.5t-190.5 286-285.5 191.5-349 71q-182 0-348-71t-286-191-191-286-71-348 71-348 191-286 286-191 348-71 348 71 286 191 191 286 71 348z"></path>
-            </svg>
-          </button>
-        </div>
-        <div>
-          <button
-            type="button"
-            class="  flex items-center justify-center w-full rounded-md  px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-50 hover:bg-gray-50 dark:hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-500"
-            id="options-menu"
-          >
-            <svg 
               class="svg-inline--fa fa-w-14 utilities-icon svg-icon-text w-[25px]"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 448 512"
@@ -96,6 +104,6 @@ export default function NavBar() {
           </button>
         </div>
       </div>
-    </nav>
+    </Navbar>
   );
 }
